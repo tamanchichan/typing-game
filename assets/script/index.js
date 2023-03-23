424533a2ecec095b0d2813c47c2d057d49327530
tamanchichan <tamanchichan@gmail.com>
2023-03-23 13:09:32 -0500

    content updated on addEventListener

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const timer = document.querySelector('.timer');
+const title = document.querySelector('.title');
 const start = document.querySelector('.start-button');
 
 function countdown() {
@@ -11,10 +12,14 @@ function countdown() {
     
     if(countdown === 0) {
       clearInterval(countdownInterval);
+      title.style.display = 'block';
+      start.style.display = 'block';
     }
   }, 1000);
 }
 
 start.addEventListener('click', () => {
+  title.style.display = 'none';
+  start.style.display = 'none';
   countdown();
 });
