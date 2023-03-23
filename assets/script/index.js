c37b89d2afeeb1f8f1926d35a7d80663fb55ee27
tamanchichan <tamanchichan@gmail.com>
2023-03-23 13:02:13 -0500

    start-button bridge  between html and js

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,13 +1,16 @@
 'use strict';
 
 const timer = document.querySelector('.timer');
+const start = document.querySelector('.start-button');
 
-let countdown = 99;
-let countdownInterval = setInterval(() => {
-  countdown--;
-  timer.innerText = `Timer: ${countdown} Seconds`;
-  
-  if(countdown === 0) {
-    clearInterval(countdownInterval);
-  }
-}, 1000);
+start.addEventListener('click', () => {
+  let countdown = 99;
+  let countdownInterval = setInterval(() => {
+    countdown--;
+    timer.innerText = `Timer: ${countdown} Seconds`;
+    
+    if(countdown === 0) {
+      clearInterval(countdownInterval);
+    }
+  }, 1000);
+})
