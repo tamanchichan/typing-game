ef06268dbb4b92cfac448cc9e1850a3489d4b2c4
tamanchichan <tamanchichan@gmail.com>
2023-03-23 12:14:19 -0500

    countdown created

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const timer = document.querySelector('.timer');
+
+let countdown = 99;
+let countdownInterval = setInterval(() => {
+  countdown--;
+  timer.innerText = `Timer: ${countdown} Seconds`;
+  
+  if(countdown === 0) {
+    clearInterval(countdownInterval);
+  }
+}, 1000);
