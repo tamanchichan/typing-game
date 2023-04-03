2a943146b49ce59522d7822c0961672d072fe547
tamanchichan <tamanchichan@gmail.com>
2023-04-03 13:45:38 -0500

    array deleted from playAgain (does not work)

--- assets/script/index.js
+++ assets/script/index.js
@@ -173,23 +173,6 @@ playAgain.addEventListener('click', () => {
   word.style.display = 'block';
   input.style.display = 'block';
   
-  const words = [
-    'dinosaur', 'love', 'pineapple', 'calendar', 'robot', 'building',
-    'population', 'weather', 'bottle', 'history', 'dream', 'character', 'money',
-    'absolute', 'discipline', 'machine', 'accurate', 'connection', 'rainbow',
-    'bicycle', 'eclipse', 'calculator', 'trouble', 'watermelon', 'developer',
-    'philosophy', 'database', 'periodic', 'capitalism', 'abominable', 'component',
-    'future', 'pasta', 'microwave', 'jungle', 'wallet', 'canada', 'coffee',
-    'beauty', 'agency', 'chocolate', 'eleven', 'technology', 'alphabet',
-    'knowledge', 'magician', 'professor', 'triangle', 'earthquake', 'baseball',
-    'beyond', 'evolution', 'banana', 'perfumer', 'computer', 'management',
-    'discovery', 'ambition', 'music', 'eagle', 'crown', 'chess', 'laptop',
-    'bedroom', 'delivery', 'enemy', 'button', 'superman', 'library', 'unboxing',
-    'bookstore', 'language', 'homework', 'fantastic', 'economy', 'interview',
-    'awesome', 'challenge', 'science', 'mystery', 'famous', 'league', 'memory',
-    'leather', 'planet', 'software', 'update', 'yellow', 'keyboard', 'window'
-  ];
-  
   player.date = new Date().toDateString().slice(3, 10);
   player.hits = 0;
   player.perc;
